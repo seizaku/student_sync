ce1fe7de521d51af3b85d973ad2c737ecea344f0
Null <seizaku.dev@gmail.com>
2025-03-08 21:52:20 +0800

    create student model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+  StudentID = models.IntegerField(primary_key=True)
+  FirstName = models.CharField(max_length=100)
+  LastName = models.CharField(max_length=100)
+  Email = models.EmailField(unique=True)
+  DateOfBirth = models.DateField()
+  Course = models.CharField(max_length=100)
+  EnrollmentDate = models.DateField()
+
